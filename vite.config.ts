6b139483a6421b84ac0dea94e956898d8d518c8f
nasirsiddiq <147470610+nasirsiddiq@users.noreply.github.com>
2026-09-01 21:52:32 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: process.env.GITHUB_ACTIONS ? '/PAK_TAX_CAL_AI_GOOGLE/' : '/',
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
